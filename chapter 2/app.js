786db2f347ac7cc21f3aa89ad7db7ce8f76eb2d1
Samina Umair <madihaumair674@gmail.com>
2026-09-10 22:07:47 +0500

    chapter two

---
+++ chapter 2/app.js
@@ -0,0 +1,48 @@
+// 1. Declare a variable called username.
+var username;
+
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+
+var fullName = "Samina Umair";
+
+
+// 3. Write script to
+// i) Declare a JS variable, titled message.
+// ii) Assign “Hello World” to variable message
+// iii) Display the message in alert box.
+
+var message;
+message = "Hello World";
+
+alert(message);
+
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+
+var studentName = "Samina Umair";
+var age = "12 years old";
+var course = "Modern Web and App Development";
+
+alert("Student Name:" + studentName);
+alert("Age:" + age);
+alert("Course:" + course);
+
+// 5. Write a script to display the following alert using one JS variable:
+
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+ alert(pizza);
+
+// 6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+
+ var email = "madihaumair674@gmail.com";
+ alert("My email address is " + email);
+
+ var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book" + book);
+
+// 8. Write a script to display this in browser through JS
+
+ document.write("Yah! I can write HTML content through JavaScript");
+
+ var line = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+ alert(line);
