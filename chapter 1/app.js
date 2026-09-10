13b41a212406d51248dc4e9a29fe417e5a30a32d
Samina Umair <madihaumair674@gmail.com>
2026-09-10 21:19:28 +0500

    chapter one

--- chapter 1/app.js
+++ chapter 1/app.js
@@ -1,12 +1,16 @@
+//.Write a script to greet your website visitor using JS alert box.
 alert("Welcome to my website");
 
+//. Write a script to display following message on your web page
 alert("Error! Please enter a valid password")
 
+//.Write a script to display following message on your web page.
 alert("Welcome to js land\n happy coding");
 
+//. Write a script to display following messages in sequence:
 alert("welcome to js land");
-
 alert("Happy coding")
 
+//. Generate the following message through browser’s developer console:
 console.log("Hello...I can run JS through my browser's console");
 
