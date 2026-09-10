33f7943e5e0c067c0bde936a96fe10cbb4743af7
Samina Umair <madihaumair674@gmail.com>
2026-09-10 21:06:46 +0500

    chapter one

---
+++ chapter 1/app.js
@@ -0,0 +1,12 @@
+alert("Welcome to my website");
+
+alert("Error! Please enter a valid password")
+
+alert("Welcome to js land\n happy coding");
+
+alert("welcome to js land");
+
+alert("Happy coding")
+
+console.log("Hello...I can run JS through my browser's console");
+
